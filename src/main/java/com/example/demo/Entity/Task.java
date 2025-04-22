c4a0b768be71fec0a0fd979e671d56318471cde3
JavaDev42 <vladimir_korolev_03@mail.ru>
2025-04-22 12:11:44 +0700

    1.0

--- src/main/java/com/example/demo/Entity/Task.java
+++ src/main/java/com/example/demo/Entity/Task.java
@@ -11,11 +11,11 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false)
     private String title;
-
+    @Column(nullable = false)
     private String description;
-
+    @Column(nullable = false)
     private LocalDateTime dueDate;
 
     @Enumerated(EnumType.STRING)
